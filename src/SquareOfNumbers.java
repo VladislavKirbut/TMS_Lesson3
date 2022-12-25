f4dfaee174155ba630d1b04b4d26e481bbc94936
Vladislav_Global <vladalexkirbut2003@gmail.com>
2022-12-25 13:33:00 +0300

    Square of numbers

---
+++ src/SquareOfNumbers.java
@@ -0,0 +1,7 @@
+public class SquareOfNumbers {
+    public static void main(String[] args) {
+        for(int i = 10; i <= 20; i++) {
+            System.out.println(i + " * " + i + " = " + (int) Math.pow(i, 2));
+        }
+    }
+}
