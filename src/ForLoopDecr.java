9b00723699d38d8c54ad55922ed76dc3800774e3
Vladislav_Global <vladalexkirbut2003@gmail.com>
2022-12-25 12:03:30 +0300

    Decrement to 1 (while loop)

---
+++ src/ForLoopDecr.java
@@ -0,0 +1,9 @@
+public class ForLoopDecr {
+    public static void main(String[] args) {
+        int i = 5;
+        while(i > 0) {
+            System.out.print(i + " ");
+            i--;
+        }
+    }
+}
