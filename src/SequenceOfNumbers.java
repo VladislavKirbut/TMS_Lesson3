5ea40bd1f15e08f5da083eaa2a6c55f33a8cb4e2
Vladislav_Global <vladalexkirbut2003@gmail.com>
2022-12-25 13:27:58 +0300

    Sequence of numbers project

---
+++ src/SequenceOfNumbers.java
@@ -0,0 +1,9 @@
+public class SequenceOfNumbers {
+    public static void main(String[] args) {
+        int i = 7;
+        while(i <= 98 ) {
+            System.out.print(i + " ");
+            i += 7;
+        }
+    }
+}
