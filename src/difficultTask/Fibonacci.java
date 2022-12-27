0d3c34b26ac748d9fce66876f8b6c5fad46f64e4
Vladislav_Global <vladalexkirbut2003@gmail.com>
2022-12-27 20:26:40 +0300

    Corrected mistakes

--- src/difficultTask/Fibonacci.java
+++ src/difficultTask/Fibonacci.java
@@ -2,14 +2,16 @@ package difficultTask;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int [] array = new int[12];
+        int [] array = new int[11];
         array[0] = 0;
         array[1] = 1;
 
-        for(int i = 2; i <= 11; i++) {
+        for(int i = 2; i < 11; i++) {
             array[i] = array[i-1] + array[i-2];
         }
 
-        System.out.println(array[11]);
+        for(int num: array) {
+            System.out.println(num);
+        }
     }
 }
