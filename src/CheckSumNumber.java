e55d5ede81ce1b33b303478e6e04dedfc0d1f764
Vladislav_Global <vladalexkirbut2003@gmail.com>
2022-12-27 20:04:51 +0300

    Corrected mistakes

--- src/CheckSumNumber.java
+++ src/CheckSumNumber.java
@@ -5,10 +5,12 @@ public class CheckSumNumber {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter random positive number: ");
         int number = scan.nextInt();
+        int sum = 0;
         if(number >= 1) {
-            for(int i = 1; i < number; i++) {
-                System.out.print(i + " ");
+            for(int i = 1; i <= number; i++) {
+                sum += i;
             }
+            System.out.println(sum);
         } else {
             System.out.println("Enter integer positive number!");
         }
